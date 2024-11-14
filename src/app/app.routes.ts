0fd806bea07f236efca6938e9a666a55e7c1d1e4
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2024-11-14 14:43:43 +0530

    first push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },  // Redirect root path to /login
+  { path: 'login', component: LoginComponent }
+
+];
