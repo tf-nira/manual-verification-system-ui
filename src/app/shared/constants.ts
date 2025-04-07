b06d9da10dc845d604d677baab46474514d127c1
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2025-04-07 21:05:43 +0530

    removed escalate button from international officer and minor fix ED role

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -28,6 +28,7 @@ export const MVS_SUPERVISOR: string = 'MVS_SUPERVISOR';
 export const MVS_DISTRICT_OFFICER: string = 'MVS_DISTRICT_OFFICER';
 export const MVS_LEGAL_OFFICER: string = 'MVS_LEGAL_OFFICER';
 export const MVS_INTERNATIONAL_OFFICER: string = 'MVS_INTERNATIONAL_OFFICER';
+export const MVS_EXECUTIVE_DIRECTOR: string ='MVS_EXECUTIVE_DIRECTOR';
 
 // table fields
 export const APPLICATION_ID: string = 'Application ID';
