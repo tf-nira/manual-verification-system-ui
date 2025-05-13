4b5bc812fcebf3cc44b3db1c9d80a9bdea82f1b5
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2025-05-13 14:56:33 +0530

    minor typographical error fix for

--- src/app/feature/application-detail/application-detail.component.ts
+++ src/app/feature/application-detail/application-detail.component.ts
@@ -154,7 +154,7 @@ export class ApplicationDetailComponent implements OnInit {
         { id: 'intoducer-details-section', label: 'Blood Relatives/Introducer Details' },
         { id: 'children-details-section', label: 'Particulars of Applicants Children' },
         { id: 'declarant-details-section', label: 'Declarants Details' },
-        { id: 'place-of-enrolmment-section', label: 'Place Of Enrollment' }
+        { id: 'place-of-enrolment-section', label: 'Place Of Enrollment' }
       ],
     },
     {
