523afb942b0d3b78ea32ca248b34bf4e8d15c399
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2025-07-24 16:41:35 +0530

    added time stamp for created date application details page

--- src/app/feature/application-detail/application-detail.component.ts
+++ src/app/feature/application-detail/application-detail.component.ts
@@ -305,9 +305,14 @@ docTitles:any;
       day: '2-digit',
       month: '2-digit',
       year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
     })
       .format(date)
-      .replace(/\//g, '-'); 
+      .replace(/\//g, '-')
+      .replace(',',''); 
 
     this.role = state.role || '';
     this.rowData = state.data || {};
