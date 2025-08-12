19966c1782e3abc2c217b3385548cbf059ce521e
Soumya Raj <soumya.raj@technoforte.co.in>
2025-08-12 16:25:49 +0530

    bug fix UNP-2246

--- src/app/feature/application-list/application-list.component.ts
+++ src/app/feature/application-list/application-list.component.ts
@@ -538,8 +538,9 @@ export class ApplicationListComponent implements OnInit {
     addFilter(this.selectedService, API_CONST_SERVICE, API_CONST_EQUALS);
     addFilter(this.selectedServiceType, API_CONST_SERVICE_TYPE, API_CONST_EQUALS);
 
-    this.selectedAgeGroups = this.selectedAgeGroups.map(age => age.split('(')[0].trim());
-    addFilter(this.selectedAgeGroups, API_CONST_AGE_GROUP, API_CONST_IN);
+    const selectedAgeGroupsFormat = this.selectedAgeGroups.map(age => age.split('(')[0].trim());
+    addFilter(selectedAgeGroupsFormat, API_CONST_AGE_GROUP, API_CONST_IN);
+    
     if (this.foundling !== null && this.foundling !== undefined) {
     const foundlingValue = this.foundling ? "Y" : "N";
     addFilter(foundlingValue, API_CONST_FOUNDLINK, API_CONST_EQUALS);
