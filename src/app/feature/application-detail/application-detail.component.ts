ee36eef28ae30a06775ccfe0ac1e11301ffdb471
Dharanidharan-S-04 <dean12345.dd@gmail.com>
2025-12-05 19:20:44 +0530

    Update application-detail.component.ts

--- src/app/feature/application-detail/application-detail.component.ts
+++ src/app/feature/application-detail/application-detail.component.ts
@@ -598,7 +598,10 @@ getTitlesForDocument(document: any): string[] {
     this.objectUrls.push(url);
   }
   getDocumentTitle(key: string): string {
-    if (key === PROOF_OF_PHYSICAL_APPLICATION_FORM && this.service && FORM_LABELS_BY_SERVICE[this.service]) {
+    if (this.serviceType=='Alien New Registration' && key === PROOF_OF_PHYSICAL_APPLICATION_FORM && this.service && FORM_LABELS_BY_SERVICE[this.service]) {
+      return FORM_LABELS_BY_SERVICE[this.serviceType];
+    }
+    else if (key === PROOF_OF_PHYSICAL_APPLICATION_FORM && this.service && FORM_LABELS_BY_SERVICE[this.service]) {
       return FORM_LABELS_BY_SERVICE[this.service];
     }
     return this.categoryMap[key] || 'Unknown Document';
