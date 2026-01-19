91dfe39cbe090677b3704219b2dc66dd85b149da
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2026-01-19 20:28:43 +0530

    Reorder sections in application detail component

--- src/app/feature/application-detail/application-detail.component.ts
+++ src/app/feature/application-detail/application-detail.component.ts
@@ -181,10 +181,9 @@ export class ApplicationDetailComponent implements OnInit {
       open: false,
       subSections: [
         { id: 'voter-info-section', label: 'Voters Information' },
-        { id: 'marital-status-section', label: 'Marital Status' },
-        { id: 'spouse-details-section', label: 'Spouse Details' },
         { id: 'immigration-section', label: 'Immigration Details'},
-        { id: 'employer-detail-section', label: 'Details of Employer/School'}
+        { id: 'marital-status-section', label: 'Marital Status' },
+        { id: 'spouse-details-section', label: 'Spouse Details' }
       ],
     },
     {
@@ -197,8 +196,9 @@ export class ApplicationDetailComponent implements OnInit {
         { id: 'intoducer-details-section', label: 'Blood Relatives/Introducer Details' },
         { id: 'children-details-section', label: 'Particulars of Applicants Children' },
         { id: 'declarant-details-section', label: 'Declarants Details' },
-        { id: 'place-of-enrolment-section', label: 'Place Of Enrollment' },
-        { id: 'LINKED_TO_DETAILS-section', label: ' Linked Information'}
+        { id: 'LINKED_TO_DETAILS-section', label: ' Linked Information'},
+        { id: 'employer-detail-section', label: 'Details of Employer/School'},
+        { id: 'place-of-enrolment-section', label: 'Place Of Enrollment' }
       ],
     },
     {
